3dea0c3ab1bd93d2dad12c1e7571b94f9c615c53
adbrown92 <adambrown1992@hotmail.com>
2025-05-15 11:42:19 +0100

    Add files via upload
    Adding more content + fixing darkmode switching etc

--- app.js
+++ app.js
@@ -30,6 +30,37 @@ function closeAllSubMenus(){
     })
 }
 
+// function switchMode() {
+//     document.body.classList.toggle('dark');
+// }
+// original code, replaced with below to make the mode stick on tab switch
+
 function switchMode() {
-    document.body.classList.toggle('dark');
-}
+         var theme = localStorage.getItem('theme');
+         var html = document.querySelector('html');
+         // choose new theme
+         if (theme === 'dark') {
+            new_theme = 'light';
+         } else {
+            new_theme = 'dark';
+         }
+         // remove previous class
+         html.classList.remove(theme);
+         // add new class
+         html.classList.add(new_theme);
+         // store theme
+         localStorage.setItem('theme', new_theme);
+      }
+
+window.addEventListener('load', function () {
+         var theme = localStorage.getItem('theme');
+         // when first load, choose an initial theme
+         if (theme === null || theme === undefined) {
+            theme = 'light';
+            localStorage.setItem('theme', theme);
+         }
+         // set theme
+         var html = document.querySelector("html");
+         // apply the variable
+         html.classList.add(theme);
+      })
